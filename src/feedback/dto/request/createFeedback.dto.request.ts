549c23aaa83323bf4a8baacc0b6709289478b160
HillsRain-ultra <hieuvu28022003@gmail.com>
2025-05-31 18:24:10 +0700

    fix id option in retrive list feedback

--- src/feedback/dto/request/createFeedback.dto.request.ts
+++ src/feedback/dto/request/createFeedback.dto.request.ts
@@ -1,7 +1,27 @@
 import { Expose, Type } from 'class-transformer';
 import { IsString, IsEnum, IsArray, IsOptional } from 'class-validator';
-import { QuestionItemDTO } from './questionItem.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { QuestionType } from 'src/entities/enums/question.type.enum';
+
+class QuestionItem {
+    @Expose()
+    @ApiProperty()
+    @IsEnum(QuestionType)
+    type: QuestionType;
+
+    @Expose()
+    @ApiProperty()
+    @IsString()
+    question: string;
+
+    @Expose()
+    @ApiProperty()
+    @IsArray()
+    @IsString({each: true})
+    @IsOptional()
+    options?: string[];
+}
+
 
 export class CreateFeedBackRequestDTO {
     @Expose()
@@ -10,10 +30,10 @@ export class CreateFeedBackRequestDTO {
 
     @Expose()
     @IsArray()
-    @Type(() => QuestionItemDTO)
+    @Type(() => QuestionItem)
     @ApiProperty({
-        type: () => [QuestionItemDTO], // chỉ rõ kiểu mảng
+        type: () => [QuestionItem], // chỉ rõ kiểu mảng
         description: 'List of questions with type and optional options',
     })
-    questions: QuestionItemDTO[];
+    questions: QuestionItem[];
 }
